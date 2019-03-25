77c79b8199fca87400bba48a2764d62af1d99029
openground <petr@chmel.net>
2019-03-25 19:36:58 +0100

    Change old style casts in tests for static_casts.

--- test/testgraph.cpp
+++ test/testgraph.cpp
@@ -10,15 +10,15 @@ TestGraph::TestGraph(QObject *parent) : QObject(parent)
 void TestGraph::test_add_vertex()
 {
     Graph g;
-    QCOMPARE(g.addVertex(), (vertex)0);
-    QCOMPARE(g.addVertex(), (vertex)1);
-    QCOMPARE(g.addVertex(), (vertex)2);
-    QCOMPARE(g.addVertex(), (vertex)3);
+    QCOMPARE(g.addVertex(), static_cast<vertex>(0));
+    QCOMPARE(g.addVertex(), static_cast<vertex>(1));
+    QCOMPARE(g.addVertex(), static_cast<vertex>(2));
+    QCOMPARE(g.addVertex(), static_cast<vertex>(3));
 
     QCOMPARE(g.addVertex(2), false);
     QCOMPARE(g.addVertex(5), true);
-    QCOMPARE(g.addVertex(), (vertex)4);
-    QCOMPARE(g.addVertex(), (vertex)6);
+    QCOMPARE(g.addVertex(), static_cast<vertex>(4));
+    QCOMPARE(g.addVertex(), static_cast<vertex>(6));
 
 }
 
@@ -31,7 +31,7 @@ void TestGraph::test_add_remove_vertex()
     g.addVertex();
 
     g.removeVertex(1);
-    QCOMPARE(g.addVertex(), (vertex)1);
+    QCOMPARE(g.addVertex(), static_cast<vertex>(1));
 }
 
 void TestGraph::test_remove_vertex()
@@ -40,11 +40,11 @@ void TestGraph::test_remove_vertex()
     g.removeVertex(0);
     g.addVertex();
     g.addVertex();
-    QCOMPARE(g.getVertices().size(), (std::size_t)2);
+    QCOMPARE(g.getVertices().size(), static_cast<std::size_t>(2));
     g.removeVertex(0);
-    QCOMPARE(g.getVertices().size(), (std::size_t)1);
+    QCOMPARE(g.getVertices().size(), static_cast<std::size_t>(1));
     g.removeVertex(0);
-    QCOMPARE(g.getVertices().size(), (std::size_t)1);
+    QCOMPARE(g.getVertices().size(), static_cast<std::size_t>(1));
 
 }
 
@@ -56,15 +56,15 @@ void TestGraph::testAddRemoveEdge()
     g.addVertex(1234);
     g.addVertex(9876);
     QCOMPARE(g.addEdge(1234, 9876), true);
-    QCOMPARE(g.getEdges().at(9876).size(), (std::size_t)1);
-    QCOMPARE(g.getEdges().at(1234).size(), (std::size_t)1);
+    QCOMPARE(g.getEdges().at(9876).size(), static_cast<std::size_t>(1));
+    QCOMPARE(g.getEdges().at(1234).size(), static_cast<std::size_t>(1));
     //The edge exists after the insertion, so it returns true
     QCOMPARE(g.addEdge(9876, 1234), true);
-    QCOMPARE(g.getEdges().at(9876).size(), (std::size_t)1);
-    QCOMPARE(g.getEdges().at(1234).size(), (std::size_t)1);
+    QCOMPARE(g.getEdges().at(9876).size(), static_cast<std::size_t>(1));
+    QCOMPARE(g.getEdges().at(1234).size(), static_cast<std::size_t>(1));
     g.removeEdge(9876, 1234);
-    QCOMPARE(g.getEdges().at(9876).size(), (std::size_t)1);
-    QCOMPARE(g.getEdges().at(1234).size(), (std::size_t)1);
+    QCOMPARE(g.getEdges().at(9876).size(), static_cast<std::size_t>(0));
+    QCOMPARE(g.getEdges().at(1234).size(), static_cast<std::size_t>(0));
 
 
 }
